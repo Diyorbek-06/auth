ff56d185d32c32658abc171e5816ac7e1bade2c0
Diyorbek-06 <diyorbekt146@gmail.com>
2025-04-30 01:06:07 +0500

    comment

--- authapp/methods/helper.py
+++ authapp/methods/helper.py
@@ -6,3 +6,7 @@ def send_to_mail(request, address , message):
     subject = "Confirmation code"
     address = address
     send_mail(subject, message, settings.EMAIL_HOST_USER, [address])
+
+
+
+    #erger
